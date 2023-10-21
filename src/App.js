fdbf29e8988eba3adf3a04c66e8b382ee556bbe8
suhailakthar18 <suhailakthar18@gmail.com>
2023-10-21 12:20:20 +0530

    commit on saturday demo

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          This is a new App...
+          This is a new App created in vvce...
           Yo!! ❤🤞💕🤷‍♀️
         </p>
       </div>
